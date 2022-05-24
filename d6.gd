93647562e5e985630fa7b5c544c7732d0abfadb2
Tim Miller <tim_j_miller@yahoo.com>
2022-05-23 20:48:11 -0400

    Set up the basic logic of the d6

---
+++ d6.gd
@@ -0,0 +1,24 @@
+extends RigidBody
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+	var x = rand_range(-10,10)
+	var y = rand_range(-10,10)
+	var z = rand_range(-10,10)
+	set_angular_velocity(Vector3(x, y, z))
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	if get_angular_velocity()==Vector3(0,0,0):
+		var face1 = $"Face1"
+		$"Viewport/Label".set_text(str(face1.global_transform.origin.z))
+		face1.set_visible(true)
+		set_process(false)
